bc3c69f6a73283fbdbf7ee9ecf217c3f996682cb
NeggFriedRice <tomloo222@gmail.com>
2024-07-07 17:58:10 +1000

    Error handling and async/await done, 4b

--- part3/part3a-notes-backend/mongo.js
+++ part3/part3a-notes-backend/mongo.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose'
+const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
     console.log('give password as argument')
@@ -20,15 +20,15 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-// const note = new Note({
-//     content: "JavaScript is super enjoyable",
-//     important: true,
-// })
+const note = new Note({
+    content: "JavaScript is super enjoyable",
+    important: true,
+})
 
-// note.save().then(result => {
-//     console.log('note saved!')
-//     mongoose.connection.close()
-// })
+note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+})
 
 Note.find({}).then(result => {
     result.forEach(note => {
