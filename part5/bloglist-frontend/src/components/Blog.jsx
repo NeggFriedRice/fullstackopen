5a1d86aa97a4921ce79c338c0b28129651627a1b
NeggFriedRice <tomloo222@gmail.com>
2024-07-24 18:24:42 +1000

    Complete exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
 
-export default Blog
+const Blog = ({ blog }) => {
+
+  const [fullView, setFullView] = useState(false)
+
+  const showFullView = {display: fullView ? 'none' : ''}
+  const hideFullView = {display: fullView ? '' : 'none'}
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={() => setFullView(true)} style={showFullView}>view</button>
+      <button onClick={() => setFullView(false)} style={hideFullView}>hide</button>
+      <div style={hideFullView}>
+        <div>
+        {blog.author}
+        </div>
+        {blog.likes}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
