2c973ee0716e7c498445136a2010d6e0f2c3e64e
NeggFriedRice <tomloo222@gmail.com>
2024-07-26 12:48:29 +1000

    Complete exercises up to 5.16

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 import { expect } from "chai";
 
@@ -21,3 +22,28 @@ test("url and likes are hidden at start", () => {
   const div = container.querySelector(".blogDetails");
   expect(div).toHaveStyle("display: none");
 });
+
+test("check that the blog's URL and likes are shown when the view button is clicked", async () => {
+  let container = render(<Blog blog={blog} />).container;
+
+  const user = userEvent.setup();
+  const button = screen.getByText("view");
+  await user.click(button);
+
+  const div = container.querySelector(".blogDetails");
+  expect(div).not.toHaveStyle("display: none");
+});
+
+test('test that button is clicked twice', async () => {
+  const mockHandler = vi.fn()
+
+  render(<Blog blog={blog} handleLike={mockHandler}/>);
+
+  const user = userEvent.setup()
+  const button = screen.getByText('like')
+  await user.click(button)
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+
+})
---
+++ part5/bloglist-frontend/src/components/NewBlog.test.jsx
@@ -0,0 +1,6 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import NewBlog from "./NewBlog"
+import { expect } from "chai";
+
+// Did not complete exercise 5.16
